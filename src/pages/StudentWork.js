8cb2b0253d3a43111b18c04ccb1f14622dd017de
Peter <trdin.peter@gmail.com>
2022-05-20 22:30:11 +0200

    #SMLTG-83 finishing up with studentWork searching

--- src/pages/StudentWork.js
+++ src/pages/StudentWork.js
@@ -14,10 +14,13 @@ function StudentWork() {
 
     const [work, setWork] = useState([]);
     const [search, setSearch] = useState('');
-    const [searchParam, setSearchParam] = useState('');
     const [distance, setDiscance] = useState('');
     const [longitude, setLongitude] = useState(0);
     const [latitude, setLatitude] = useState(0);
+    const [payFrom, setPayFrom] = useState('');
+    const [payTo, setPayTo] = useState('');
+    const [searchError, setSearchError] = useState('');
+
 
     const getLocation = function () {
         console.log(longitude)
@@ -27,7 +30,6 @@ function StudentWork() {
             setLatitude(position.coords.latitude)
         });
     }
-    const HandleChangeSearch = function (e) { setSearch(e.target.value) }
     getLocation();
 
 
@@ -46,20 +48,32 @@ function StudentWork() {
 
         getLocation();
 
-        const res = await fetch('http://localhost:3000/studentWork/search', {
-            method: 'POST',
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                search: search,
-                latitude: latitude,
-                longitude: longitude,
-                distance: distance,
-            })
-        }).catch(errror => { console.error(errror); });
-        const data = await res.json();
-        setWork(data);
-        //setSearchParam(search);
+        if (isNaN(distance) && distance != "") {
+            setSearchError("Distance must be a number")
+        } else if (isNaN(payFrom) && payFrom != "") {
+            setSearchError("pay from must be a number")
+        } else if (isNaN(payTo) && payTo != "") {
+            setSearchError("pay to must be a number")
+        } else {
+            const res = await fetch('http://localhost:3000/studentWork/search', {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    search: search,
+                    latitude: latitude,
+                    longitude: longitude,
+                    distance: distance,
+                    payFrom: payFrom,
+                    payTo: payTo
+                })
+            }).catch(errror => { console.error(errror); });
+            const data = await res.json();
+            if (data != undefined) {
+                setWork(data);
+            }
+            setSearchError("")
+        }
     }
 
 
@@ -70,21 +84,34 @@ function StudentWork() {
                 <div className="container">
                     <form onSubmit={Search} className="form-inline my-2 my-lg-0">
                         <div className="form-group">
-                            <input className="form-control mr-sm-2" type="search" name="search" placeholder="Search by type" aria-label="Search" value={search} onChange={HandleChangeSearch} />
+                            <input className="form-control mr-sm-2" type="search" name="search" placeholder="Search by type" aria-label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
                         </div>
                         <div className="form-group">
                             {
                                 longitude == 0 && latitude == 0 ?
-                                    <input className="form-control mr-sm-2" type="search" name="search by type" placeholder="Enable location" aria-label="Enable location" disabled />
+                                    <input className="form-control mr-sm-2" type="search" name="location" placeholder="Enable location" aria-label="Enable location" disabled />
                                     :
-                                    <input className="form-control mr-sm-2" type="search" name="search by type" placeholder="Input distance" aria-label="Search" value={distance} onChange={(e) => { setDiscance(e.target.value) }} />
+                                    <input className="form-control mr-sm-2" type="search" name="location" placeholder="Input distance" aria-label="Search" value={distance} onChange={(e) => { setDiscance(e.target.value) }} />
 
                             }
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control mr-sm-2" type="search" name="search" placeholder="Pay neto from" aria-label="Search" value={payFrom} onChange={(e) => setPayFrom(e.target.value)} />
+                        </div><div className="form-group">
+                            <input className="form-control mr-sm-2" type="search" name="search" placeholder="Pay neto to" aria-label="Search" value={payTo} onChange={(e) => setPayTo(e.target.value)} />
                         </div>
                         <div className="form-group">
                             <button className="btn btn-success my-2 my-sm-0" type="submit" >Search by tag</button>
                         </div>
                     </form>
+                    {searchError != "" ?
+                        <div className="alert alert-danger mt-3" role="alert">
+                            {searchError}
+                        </div>
+                        : ""
+                    }
+
+
                 </div>
             </div>
             <div className="row">

ca1b8c4ea648aad4f469a2191f4c087d46b7d6ea
UMtrdin <peter.trdin@student.um.si>
2022-06-03 22:44:33 +0200

    #SMLTG-109 fixing events

--- src/components/frames/EventFrame.js
+++ src/components/frames/EventFrame.js
@@ -7,11 +7,19 @@ function EventFrame(props) {
         <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="jumbotron jumbotron-fluid dataContainer text-center shadow-sm">
                 <div className="container">
-                    <h5 className="text-center" style={{ height: 50 }}>{event.title}</h5>
-                    <h6>{event.content}</h6>
-                    <h6>{event.address}</h6>
-                    <h6>start date: {start}</h6>
-                    <a className="btn btn-primary btn-lg" href={event.url} role="button">Read more</a>
+                    <h5 className="text-center" style={{ height: 70 }}>{event.title}</h5>
+                    
+                    <h6 className="mb-3" style={{ height: 30 }}>{event.address}</h6>
+                    <h6 style={{ height: 30 }}>start date: {start}</h6>
+                    <button className="btn darkBackground text-white mb-3 mt-3" type="button" data-toggle="collapse" data-target={`#collapseExample${event._id}`} aria-expanded="false" aria-controls="collapseExample">
+                        Podrobnosti
+                    </button>
+                    <div className="collapse" id={`collapseExample${event._id}`}>
+                        <h6 className="mb-3">{event.content}</h6>
+
+                        <a className="btn darkBackground text-white" href={event.url} role="button">Preberi več</a>
+
+                    </div>
 
                 </div>
             </div>

a1e7998f0d1bcf0d18f8366eda85e852416e2968
Peter <trdin.peter@gmail.com>
2022-06-05 19:41:08 +0200

    #SMLTG-109 fixing dates and hours events

--- src/components/frames/EventFrame.js
+++ src/components/frames/EventFrame.js
@@ -1,11 +1,18 @@
 
 function EventFrame(props) {
     var event = props.event;
-    console.log(event);
-    console.log(event.start)
-    //var string = start.toLocaleDateString('sl-si')
-    // console.log(string);
-    var start = Intl.DateTimeFormat('en', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(Date.parse(event.start))
+    if (event.start != undefined) {
+        var start = new Date(event.start);
+        start.setHours(start.getHours() - 2);
+
+        start = Intl.DateTimeFormat('sl-si', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(start);
+    }
+    if (event.end != undefined || event.end != '') {
+        var end = new Date(event.end);
+        end.setHours(end.getHours() - 2);
+        var end = Intl.DateTimeFormat('sl-si', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(end)
+    }
+
     return (
         <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="jumbotron jumbotron-fluid dataContainer text-center shadow-sm">
@@ -13,8 +20,11 @@ function EventFrame(props) {
                     <h5 className="text-center" style={{ height: 70 }}>{event.title}</h5>
 
                     <h6 className="mb-3" style={{ height: 30 }}>{event.address}</h6>
-                    <h6 style={{ height: 30 }}>start date: {start}</h6>
-                    <h6 style={{ height: 30 }}>end date: {event.end}</h6>
+                    <h6 style={{ height: 30 }}>Začetek dogodka: {start}</h6>
+                    {end ?
+                        <h6 style={{ height: 30 }}>Konec dogodka: {end}</h6>
+                        : ''
+                    }
                     <button className="btn darkBackground text-white mb-3 mt-3" type="button" data-toggle="collapse" data-target={`#collapseExample${event._id}`} aria-expanded="false" aria-controls="collapseExample">
                         Podrobnosti
                     </button>
